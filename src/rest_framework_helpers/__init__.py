5519015b578f42e3ea8a40cf8b32f52c1fa25034
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-29 10:58:06 -0400

    chore: 0.2.1.
    Updates the version from 0.2.0 to 0.2.1.

--- src/rest_framework_helpers/__init__.py
+++ src/rest_framework_helpers/__init__.py
@@ -1,4 +1,4 @@
 # Do not change this version manually.
 # Versioning is managed by package_controller.
 # To update the version run `pc version --patch | --minor | --major`
-__version__ = "0.2.0"
+__version__ = "0.2.1"
