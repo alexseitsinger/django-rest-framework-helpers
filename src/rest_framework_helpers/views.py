cf910616d23cbc46abea564f9f5f7b3c30170199
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-22 17:50:25 -0400

    feat: Adds view name formatting.

--- src/rest_framework_helpers/views.py
+++ src/rest_framework_helpers/views.py
@@ -8,6 +8,8 @@ class APIRootView(APIView):
     endpoints = {}
     endpoints_seen = {}
     endpoints_created = None
+    view_name_prefix = ""
+    view_name_suffix = "-list"
 
     def is_endpoints_changed(self, endpoints, seen, created):
         changed = False
@@ -21,10 +23,26 @@ class APIRootView(APIView):
                 changed = True
         return changed
 
+    def get_full_view_name(self, name):
+        prefix = self.view_name_prefix
+        suffix = self.view_name_suffix
+        if not name.startswith(prefix):
+            if prefix.endswith(":"):
+                prefix = prefix[:-1]
+            name = "{}:{}".format(prefix, name)
+        if not name.endswith(suffix):
+            if suffix.startswith("-"):
+                suffix = suffix[1:]
+            name = "{}-{}".format(name, suffix)
+        name = name.replace(r"-+", "-")
+        name = name.replace(r":+", ":")
+        return name
+
     def create_endpoints(self, request, format, endpoints):
         created = OrderedDict()
         for name, view_name in endpoints.items():
-            created[name] = reverse(view_name, request=request, format=format)
+            full_view_name = self.get_full_view_name(view_name)
+            created[name] = reverse(full_view_name, request=request, format=format)
         return created
 
     def get_endpoints(self, request, format):
