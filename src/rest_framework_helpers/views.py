d73a2d072790553d1181a7bee8861c994fd0833b
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-22 17:29:06 -0400

    refactor: Updates APIRootView.

--- src/rest_framework_helpers/views.py
+++ src/rest_framework_helpers/views.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.http import Http404
 from collections import OrderedDict
 from rest_framework.reverse import reverse
 from rest_framework.response import Response
@@ -8,18 +6,35 @@ from rest_framework.views import APIView
 
 class APIRootView(APIView):
     views = {}
+    views_seen = {}
+    endpoints = None
 
-    def create_endpoints(self, request, format):
+    def is_views_changed(self, endpoints, views, seen):
+        changed = False
+        if endpoints is None:
+            changed = True
+        for name, view_name in views.items():
+            if changed is True:
+                break
+            if name not in seen or seen[name] != view_name:
+                seen[name] = view_name
+                changed = True
+        return changed
+
+    def create_endpoints(self, request, format, views):
         endpoints = OrderedDict()
-        for endpoint_name, view_name in self.views.items():
-            endpoints[endpoint_name] = reverse(
-                view_name, request=request, format=format
-            )
+        for name, view_name in views.items():
+            endpoints[name] = reverse(view_name, request=request, format=format)
+        return endpoints
+
+    def get_endpoints(self, request, format):
+        endpoints = self.endpoints
+        views = self.views
+        seen = self.views_seen
+        changed = self.is_views_changed(endpoints, views, seen)
+        if changed is True:
+            endpoints = self.endpoints = self.create_endpoints(request, format, views)
         return endpoints
 
     def get(self, request, format=None):
-        if settings.IS_DEVELOPMENT:
-            endpoints = self.create_endpoints(request, format)
-            return Response(endpoints)
-        else:
-            raise Http404()
+        return Response(self.get_endpoints(request, format))
