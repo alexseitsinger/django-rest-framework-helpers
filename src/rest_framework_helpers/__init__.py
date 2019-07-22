a823595434f0a1b7d438e944c8a321917ee45214
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-22 18:04:11 -0400

    chore: 5.2.1.
    Updates the version from 5.2.0 to 5.2.1.

--- src/rest_framework_helpers/__init__.py
+++ src/rest_framework_helpers/__init__.py
@@ -1,4 +1,4 @@
 # Do not change this version manually.
 # Versioning is managed by package_controller.
 # To update the version run `pc version --patch | --minor | --major`
-__version__ = "5.2.0"
+__version__ = "5.2.1"
