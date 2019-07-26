c9e3ef8e8a2c896c16646d2e0bc46ca855865e2f
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-26 07:20:08 -0400

    chore: 6.0.0.
    Updates the version from 5.3.0 to 6.0.0.

--- src/rest_framework_helpers/__init__.py
+++ src/rest_framework_helpers/__init__.py
@@ -1,4 +1,4 @@
 # Do not change this version manually.
 # Versioning is managed by package_controller.
 # To update the version run `pc version --patch | --minor | --major`
-__version__ = "5.3.0"
+__version__ = "6.0.0"
