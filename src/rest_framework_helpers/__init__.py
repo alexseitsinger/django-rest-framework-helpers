e889e0f510e2a913cb0afde5c08e4a4fd1ae442d
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-08-25 03:16:23 -0400

    chore: 7.1.0.
    Updates the version from 7.0.3 to 7.1.0.

--- src/rest_framework_helpers/__init__.py
+++ src/rest_framework_helpers/__init__.py
@@ -1,4 +1,4 @@
 # Do not change this version manually.
 # Versioning is managed by package_controller.
 # To update the version run `pc version --patch | --minor | --major`
-__version__ = "7.0.3"
+__version__ = "7.1.0"
